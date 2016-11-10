dab8524d2956446fbd431e72d630d8033f802155
Sébastien Prunier <sebastien.prunier@gmail.com>
2016-11-10 17:12:34 +0100

    Add name function is User model

--- app/models/user.scala
+++ app/models/user.scala
@@ -4,4 +4,5 @@ import play.api.libs.json.JsValue
 
 case class User(profile: JsValue) {
   def email: String = (profile \ "email").as[String]
+  def name: Option[String] = (profile \ "name").asOpt[String]
 }
