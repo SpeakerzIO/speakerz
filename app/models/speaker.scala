d42d72609f1c5d757399bd751248fdf96e11d0eb
Mathieu <mathieu.ancelin@gmail.com>
2017-01-10 14:47:53 +0100

    Add logs

--- app/models/speaker.scala
+++ app/models/speaker.scala
@@ -85,6 +85,7 @@ object Speaker {
   def update(speaker: Speaker)(implicit ec: ExecutionContext): Future[Unit] = {
     Future {
       DB.withConnection { implicit c =>
+        Logger.info(s"Update speaker $speaker")
         SQL("update Speakerz set document = {document}::json where id = {id}")
           .on("id" -> speaker.id, "document" -> Json.stringify(speaker.toJson))
           .executeUpdate()
